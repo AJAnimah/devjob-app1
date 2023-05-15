7da75d236461d0c7fff3781719b1ae5141adff66
Lindamell Abadoo <linmell2010@gmail.com>
2023-05-15 14:09:21 +0000

    changes made to files and folders

--- README.md
+++
@@ -1,1 +0,0 @@
-# devjob-app1
---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1
+        style={{
+          height: "80vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "40px",
+          color: "brown",
+        }}
+      >
+        Hurray!!! Happy building
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
